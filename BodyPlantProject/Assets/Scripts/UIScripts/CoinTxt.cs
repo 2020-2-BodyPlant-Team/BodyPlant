13d0805f972b319794ad0a6a3d63da98016a0fd4
ahnw00 <ahnw00@naver.com>
2021-01-05 16:25:57 +0900

    상점 팝업창 만들기(기본적인것만)

---
+++ BodyPlantProject/Assets/Scripts/UIScripts/CoinTxt.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinTxt : MonoBehaviour
+{
+
+    [SerializeField] Text CoinText;
+    GameManager gameManager;        //게임매니저를 통해 세이브데이터를 참조해야 한다.
+    SaveDataClass saveData;         //게임매니저를 통해 가져올 세이브 데이터
+    // Start is called before the first frame update
+    void Start()
+    {
+        CoinText.text = saveData.coin.ToString() + "c";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void substractionToCoin()
+    {
+        
+    }
+}
---
+++ BodyPlantProject/Assets/Scripts/UIScripts/StoreBtn.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoreBtn : MonoBehaviour
+{
+    [SerializeField] GameObject PopUp;  // 열고 닫을 팝업창
+    [SerializeField] bool isOpenBtn;  // 팝업창을 여는 버튼일 경우 true로, 아닌 경우 false로
+
+    public void OnClick_Btn()
+    {
+        // 만약 팝업창을 여는 버튼일 경우 꺼져있는 팝업창을 켜준다.
+        if(isOpenBtn)
+        {
+            PopUp.SetActive(true);
+        }
+        // 만약 팝업창을 닫는 버튼일 경우 켜져있는 팝업창을 닫아준다.
+        else
+        {
+            PopUp.SetActive(false);
+        }
+    }
+}
