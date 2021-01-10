394999a946668cf874936ea68ae57f1aa2b17dd0
Wadaha <63825388+Jeong-Sanghun@users.noreply.github.com>
2021-01-10 11:48:04 +0900

    도감 만들다 맘ㅁ
    만들다 말았어요

---
+++ BodyPlantProject/Assets/Scripts/ManagerScript/BookManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookManager : MonoBehaviour
+{
+    GameManager gameManager;
+    SaveDataClass saveData;
+    List<CharacterClass> characterList;
+    public GameObject canvasPrefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameManager.singleTon;
+        saveData = gameManager.saveData;
+        characterList = saveData.characterList;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
