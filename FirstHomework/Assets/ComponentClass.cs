4c34ab211e766dedc24808391bb779b8406dd141
Wadaha <63825388+Jeong-Sanghun@users.noreply.github.com>
2021-01-11 23:11:19 +0900

    첫 번째 과제파일

---
+++ FirstHomework/Assets/ComponentClass.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComponentClass
+{
+    public string name;         //이름
+    public Vector2 position;             //부위의 위치     
+    public GameObject realGameobject;      //이 컴포넌트가 가진 실제의 게임오브젝트.
+    public float sproutTime;         //피워나는데 걸리는 시간
+    public SpriteRenderer spriteRenderer;   //스프라이트 렌더러 component. 나중에 realGameObject.GetComponent<SpriteRenderer>()해야함.
+    public float timer = 0;
+
+    public ComponentClass()         //요건 생성자라는겁니다. 객체를 생성할때 기본값을 지정해주는, 자동실행되는 함수입니다.
+    {
+        position = Vector2.zero;
+
+        sproutTime = 10;
+    }
+
+}
---
+++ FirstHomework/Assets/GameManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    int index = 0;
+    List<ComponentClass> componentsList;
+    List<float> timerList;
+    public GameObject imageObject;
+    public Sprite unSproutedSprite;
+    public Sprite sproutedSprite;
+    float timer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        componentsList = new List<ComponentClass>();
+        timerList = new List<float>();
+    }
+
+
+    // Update is called once per frame
+   
+    void Update()
+    {
+
+        //update한번 돌아갈 때 걸리는 시간.
+        
+        for(int i = 0; i < index; i++)
+        {
+            timerList[i] += Time.deltaTime;
+            if(timerList[i] > componentsList[i].sproutTime)
+            {
+                componentsList[i].spriteRenderer.sprite = sproutedSprite;
+                //componentsList[i].realGameobject.SetActive(false);
+            }
+        }
+
+
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            componentsList.Add(new ComponentClass());
+            timerList.Add(0);
+
+            componentsList[index].realGameobject = Instantiate(imageObject);
+            componentsList[index].spriteRenderer = componentsList[index].realGameobject.GetComponent<SpriteRenderer>();
+            componentsList[index].spriteRenderer.sprite = unSproutedSprite;
+
+            componentsList[index].realGameobject.transform.position = new Vector3(index, 0, 0);
+            index++;
+            //10초가 지나면 sproutedSprite로 sprite가 바뀐다
+            
+        }
+
+        
+    }
+}
