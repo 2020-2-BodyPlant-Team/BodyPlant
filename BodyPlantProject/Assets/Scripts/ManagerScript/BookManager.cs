cad03ac24e8413bdd225c29e7fa8b6ee7d558927
ahnw00 <ahnw00@naver.com>
2021-01-30 18:51:48 +0900

    asdf

--- BodyPlantProject/Assets/Scripts/ManagerScript/BookManager.cs
+++ BodyPlantProject/Assets/Scripts/ManagerScript/BookManager.cs
@@ -8,19 +8,19 @@ public class BookManager : MonoBehaviour
     GameManager gameManager;
     SaveDataClass saveData;
     List<CharacterClass> characterList;
-    public GameObject DiaryPrefab;
-    public GameObject ButtonPrefab;
+    public GameObject diaryPrefab;
+    public GameObject buttonPrefab;
     List<GameObject> prefabList;
     List<GameObject> buttonList;
     List<int> passedTime;
     int buttonXgap = 300;
     int buttonYgap = -415;    
-    Vector3 buttonStartPoint = new Vector3(-300, 370, 0);
-    public GameObject Parent;
+    Vector2 buttonStartPoint = new Vector2(-300, 370);
+    public GameObject parent;
 
 
     // Start is called before the first frame update
-    void Awake()
+    void Start()
     {
         
         gameManager = GameManager.singleTon;
@@ -31,42 +31,40 @@ public class BookManager : MonoBehaviour
         buttonList = new List<GameObject>();
 
         for(int i = 0; i < characterList.Count; i++)
-        {
-            
-            
-            prefabList.Add(Instantiate(DiaryPrefab, Parent.transform));
-                               
-            buttonList.Add(Instantiate(ButtonPrefab, Parent.transform));
-            
-            
+        { 
+            prefabList.Add(Instantiate(diaryPrefab, parent.transform));                   
+            buttonList.Add(Instantiate(buttonPrefab, parent.transform));
+            buttonList[i].GetComponent<RectTransform>().anchoredPosition = new Vector2((buttonStartPoint.x + (i % 3) * buttonXgap), (buttonStartPoint.y  + (int)(i / 3) * buttonYgap));
+            Button button = buttonList[i].GetComponent<Button>();
+            button.onClick.AddListener(delegate { ButtonFunction(); });
 
             prefabList[i].transform.GetChild(1).GetComponent<Text>().text = characterList[i].createdDate;
             //prefabList[i].transform.GetChild(2).GetComponent<Text>().text = characterList[i].personality;
             prefabList[i].transform.GetChild(3).GetComponent<Text>().text = characterList[i].loveNess.ToString();
-            prefabList[i].transform.GetChild(4).GetComponent<Text>().text = characterList[i].name;
-            
-                      
+            prefabList[i].transform.GetChild(4).GetComponent<Text>().text = characterList[i].name;              
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        ButtonFunction();
+        
     }
 
     public void ButtonFunction()
     {
         for(int i = 0; i < characterList.Count; i++)
         {
-            if(Input.GetKeyDown(KeyCode.Mouse0) == buttonList[i])
+            if(prefabList[i] == false)
             {
-                if(prefabList[i] == false)
-                {
-                    prefabList[i].SetActive(true);
-                }
+                prefabList[i].SetActive(true);
             }
         }
+    }
+
+    public void HouseSceneLoad()
+    {
+        gameManager.HouseSceneLoad();
     }
     
 }
