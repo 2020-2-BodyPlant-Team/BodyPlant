40df041427c73ea508e092840649782b2f203106
ahnw00 <ahnw00@naver.com>
2021-02-13 17:38:54 +0900

    bookScene

--- BodyPlantProject/Assets/Scripts/ManagerScript/BookManager.cs
+++ BodyPlantProject/Assets/Scripts/ManagerScript/BookManager.cs
@@ -17,8 +17,10 @@ public class BookManager : MonoBehaviour
     List<int> passedTime;
     int buttonXgap = 300;
     int buttonYgap = -415;    
-    Vector2 buttonStartPoint = new Vector2(-300, 370);
-    public GameObject parent;
+    Vector2 buttonStartPoint = new Vector2(-300, -100);
+    public GameObject buttonParent;
+    public GameObject diaryParent;
+    public RectTransform contentRect;
 
 
     // Start is called before the first frame update
@@ -31,12 +33,16 @@ public class BookManager : MonoBehaviour
         diaryList = new List<GameObject>();
         buttonList = new List<GameObject>();
 
+        contentRect.anchoredPosition = new Vector2(0, 0);   
+        contentRect.sizeDelta = new Vector2(0, (characterList.Count / 3) * (-buttonYgap));
+        buttonStartPoint = new Vector2(-300, contentRect.sizeDelta.y / 2 - 180);
+
         for(int i = 0; i < characterList.Count; i++)
         { 
             int elapsedTime;
 
-            diaryList.Add(Instantiate(diaryPrefab, parent.transform));                   
-            buttonList.Add(Instantiate(buttonPrefab, parent.transform));
+            diaryList.Add(Instantiate(diaryPrefab, diaryParent.transform));                   
+            buttonList.Add(Instantiate(buttonPrefab, buttonParent.transform));
 
             Vector2 buttonPosition = new Vector2((buttonStartPoint.x + (i % 3) * buttonXgap), (buttonStartPoint.y  + (int)(i / 3) * buttonYgap));
             buttonList[i].GetComponent<RectTransform>().anchoredPosition = buttonPosition;
@@ -65,11 +71,21 @@ public class BookManager : MonoBehaviour
             }
             
             diaryList[i].transform.GetChild(3).GetComponent<Text>().text = characterList[i].loveNess.ToString();
-            diaryList[i].transform.GetChild(4).GetComponent<Text>().text = characterList[i].name + " 와(과) 지낸지";
+            diaryList[i].transform.GetChild(4).GetComponent<Text>().text = characterList[i].name;
 
             elapsedTime = gameManager.TimeSubtractionToSeconds(characterList[i].createdDate, DateTime.Now.ToString());
             int days = elapsedTime / (60 * 60 * 24);
-            diaryList[i].transform.GetChild(5).GetComponent<Text>().text = days.ToString() + " 일이 지났어";              
+            diaryList[i].transform.GetChild(5).GetComponent<Text>().text = days.ToString();             
+                GameObject parent = new GameObject();
+                parent.transform.SetParent(diaryList[i].transform);
+                parent.transform.localPosition = new Vector3(0,0,-1);
+            for(int k = 0;k<characterList[i].components.Count;k++){
+                ComponentClass component = characterList[i].components[k];
+                GameObject prefab = Resources.Load<GameObject>("Components/Complete/" + component.name);
+                GameObject inst = Instantiate(prefab,parent.transform);
+                inst.transform.localPosition = component.position;
+                inst.transform.eulerAngles = component.rotation;
+            } 
         }
 
         for(int i = 0; i < characterList.Count; i++)
