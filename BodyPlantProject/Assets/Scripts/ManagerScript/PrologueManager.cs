2e2182293b9db94ce75b33f717c54733344413cb
Wadaha <63825388+Jeong-Sanghun@users.noreply.github.com>
2021-09-06 09:50:36 +0900

    프롤로그 완료 및 앱아이콘, 스플래시 이미지

--- BodyPlantProject/Assets/Scripts/ManagerScript/PrologueManager.cs
+++ BodyPlantProject/Assets/Scripts/ManagerScript/PrologueManager.cs
@@ -26,10 +26,13 @@ public class PrologueManager : MonoBehaviour
     Vector3[] originEulerAngles;
     bool[] buttonActive;
     bool magnified =false;
-
+    
+    [SerializeField ]
     int nowLittleButtonIndex;
+    [SerializeField]
     int nowImageIndex;
     bool buttonClickable;
+    bool afterCameraOrigin;
 
     bool isInBook = true;
     [SerializeField]
@@ -143,7 +146,7 @@ public class PrologueManager : MonoBehaviour
         image.gameObject.SetActive(true);
         while (timer < 1)
         {
-            timer += Time.deltaTime/2;
+            timer += Time.deltaTime;
             image.color = new Color(1, 1, 1, timer);
             yield return null;
         }
@@ -158,7 +161,7 @@ public class PrologueManager : MonoBehaviour
     {
         soundManager.OpenPaperEffectPlay();
         float timer = 0;
-        nowImageIndex = 0;
+        nowImageIndex = 1;
         RectTransform rect = littleButtonArray[nowLittleButtonIndex].GetComponent<RectTransform>();
         for(int i = 0; i < 4; i++)
         {
@@ -191,6 +194,8 @@ public class PrologueManager : MonoBehaviour
             littleButtonArray[nowLittleButtonIndex].transform.localScale = new Vector3(1, 1, 1) * (1+timer*2);
             yield return null;
         }
+
+
         littleButtonArray[nowLittleButtonIndex].SetActive(false);
         canvasArray[nowLittleButtonIndex].SetActive(true);
 
@@ -211,6 +216,7 @@ public class PrologueManager : MonoBehaviour
         buttonClickable = false;
         RectTransform rect = canvasArray[nowLittleButtonIndex].GetComponent<RectTransform>();
         Vector3 origin = rect.anchoredPosition;
+        Vector3 originScale = rect.localScale;
 
 
         //littleButtonArray[nowLittleButtonIndex].transform.GetChild(0).gameObject.SetActive(false);
@@ -220,7 +226,8 @@ public class PrologueManager : MonoBehaviour
             timer += Time.deltaTime;
             canvasArray[nowLittleButtonIndex].transform.localEulerAngles = Vector3.Lerp( Vector3.zero, originEulerAngles[nowLittleButtonIndex], timer);
             rect.anchoredPosition = Vector3.Lerp( origin, originPos[nowLittleButtonIndex], timer);
-            canvasArray[nowLittleButtonIndex].transform.localScale = new Vector3(1, 1, 1) * (1- timer * 2.0f/3.0f);
+            //canvasArray[nowLittleButtonIndex].transform.localScale = originScale * (1 - timer * 2.0f / 3.0f);
+            canvasArray[nowLittleButtonIndex].transform.localScale = Vector3.Lerp(originScale, Vector3.one * 1f / 3f, timer);
             //if(nowLittleButtonIndex == 3)
             //{
             //    fourthText.fontSize = (int)(49 / (timer*2 + 1));
@@ -231,7 +238,7 @@ public class PrologueManager : MonoBehaviour
         canvasArray[nowLittleButtonIndex].SetActive(false);
 
         canvasArray[nowLittleButtonIndex].transform.localEulerAngles = originEulerAngles[nowLittleButtonIndex];
-        canvasArray[nowLittleButtonIndex].transform.localScale = new Vector3(1, 1, 1) * 2.0f/3.0f;
+        canvasArray[nowLittleButtonIndex].transform.localScale = new Vector3(1, 1, 1) * 1.0f/3.0f;
         canvasArray[nowLittleButtonIndex].SetActive(false);
         for (int i = 0; i < nowLittleButtonIndex+1; i++)
         {
@@ -266,6 +273,7 @@ public class PrologueManager : MonoBehaviour
     IEnumerator CameraWork()
     {
         cameraMoving =true;
+        afterCameraOrigin = false;
         float timer = 0;
         timer = 0;
         Vector3 origin = new Vector3(-500, 0, 0);
@@ -277,6 +285,89 @@ public class PrologueManager : MonoBehaviour
             yield return null;
         }
         spriteParentRectArray[nowLittleButtonIndex].anchoredPosition = goal;
+
+
+        while (true)
+        {
+            if (Input.GetMouseButtonDown(0) )
+            {
+                Debug.Log("인풋");
+                if(nowImageIndex >=4 && nowLittleButtonIndex == 1)
+                {
+                    Debug.Log("브레이크");
+                    break;
+                }
+                if(nowImageIndex >=2 && nowLittleButtonIndex == 2)
+                {
+                    break;
+                }
+                
+            }
+            yield return null;
+        }
+
+        origin = new Vector3(500, 0, 0);
+        goal = new Vector3(-500, 0, 0);
+        timer = 0;
+        while (timer < 1)
+        {
+            timer += Time.deltaTime * 0.3f;
+            spriteParentRectArray[nowLittleButtonIndex].anchoredPosition = Vector2.Lerp(origin, goal, timer);
+            yield return null;
+        }
+        spriteParentRectArray[nowLittleButtonIndex].anchoredPosition = goal;
+        if (nowLittleButtonIndex == 1 && nowImageIndex == 4)
+        {
+            spriteArray[nowLittleButtonIndex,nowImageIndex].SetActive(true);
+        }
+
+
+        while (true)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (nowImageIndex >= 4 && nowLittleButtonIndex == 1)
+                {
+                    break;
+                }
+                if (nowLittleButtonIndex == 2)
+                {
+                    break;
+                }
+
+            }
+            yield return null;
+        }
+        timer = 0;
+        origin = new Vector3(-500, 0, 0);
+        goal = Vector3.zero;
+        while (timer < 1)
+        {
+            timer += Time.deltaTime * 0.6f;
+            spriteParentRectArray[nowLittleButtonIndex].anchoredPosition = Vector2.Lerp(origin, goal, timer);
+            spriteParentRectArray[nowLittleButtonIndex].localScale = Vector3.Lerp(Vector3.one, Vector3.one / 2f, timer);
+            yield return null;
+        }
+
+        while (true)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (nowImageIndex >= 4 && nowLittleButtonIndex == 1)
+                {
+                    break;
+                }
+                if (nowLittleButtonIndex == 2)
+                {
+                    break;
+                }
+
+            }
+            yield return null;
+        }
+        nowImageIndex++;
+
+        afterCameraOrigin = true;
         cameraMoving = false;
         if(returnAfterCamera == true)
         {
@@ -363,7 +454,7 @@ public class PrologueManager : MonoBehaviour
                         //littleButtonArray[nowLittleButtonIndex].GetComponent<Image>().sprite = firstSpriteArray[firstSpriteArray.Length - 1].GetComponent<Image>().sprite;
                         if (cameraMoving || nowTexting)
                         {
-                            returnAfterCamera = true;
+                            //returnAfterCamera = true;
                         }
                         else
                         {
@@ -376,6 +467,11 @@ public class PrologueManager : MonoBehaviour
                 }
                 else
                 {
+                    if(nowLittleButtonIndex == 1 && nowImageIndex == 4 && !afterCameraOrigin)
+                    {
+                        Debug.Log("리턴");
+                        return;
+                    }
                     spriteArray[nowLittleButtonIndex,nowImageIndex].SetActive(true);
                     if(nowLittleButtonIndex == 0)
                     {
@@ -387,7 +483,7 @@ public class PrologueManager : MonoBehaviour
                     }
                     if (nowLittleButtonIndex == 3 && !nowTexting)
                     {
-                        StartCoroutine(LoadTextOneByOne(textString.text, fourthText));
+                        StartCoroutine(LoadTextOneByOne(textString.text, fourthText,0.1f));
                     }
                     nowImageIndex++;
                 }
